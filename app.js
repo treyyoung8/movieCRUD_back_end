5a22b106cd2577b849b30c6fcf1d0ca90bc48d26
= <treyyoung8@gmail.com>
2018-12-06 15:19:35 -0700

    minor change

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ const movies = require('./routes/movies_routes')
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use('/', movies)
+app.use('/movies', movies)
 
 app.listen(port, console.log(`yo ${port}`))
